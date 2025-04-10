02c88d040c4199756d59f98f3bbf894896065156
DevDiscere <dev.ginomiguel@gmail.com>
2025-04-10 16:41:09 +0800

    Add basic math operations using functions
    I added the basic math operations like add, subtract, multiply, and
    divide using functions in JavaScript. For the division operation, I
    added a validation to the second operand that checks if it is zero,
    so that I can return a custom message about how the user cannot divide
    by zero.

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-para = document.querySelector("p");
-para.style.backgroundColor = "blue";
+const add = function add (firstOperand, secondOperand) {
+    return firstOperand + secondOperand;
+}
+
+const subtract = function subtract (firstOperand, secondOperand) {
+    return firstOperand - secondOperand;
+}
+
+const multiply = function multiply (firstOperand, secondOperand) {
+    return firstOperand * secondOperand;
+}
+
+const divide = function divide (firstOperand, secondOperand) {
+    if (secondOperand === 0) return "Can't divide by zero."
+    return firstOperand / secondOperand;
+}
+
+console.log(add(3, 5));
+console.log(subtract(3, 5));
+console.log(multiply(3, 5));
+console.log(divide(1, 0));
+console.log(divide(0, 1));
