92e2e534c63c15220428fb3253462afc76e64f93
DevDiscere <dev.ginomiguel@gmail.com>
2025-04-13 13:08:08 +0800

    Add getCalculatorInputs and displayResult function
    I added the logic for taking the user's calculator inputs, through the
    buttons, and storing them in global variable holders. To determine the
    textContent passed by the button, I utilized arrays to efficiently
    hold references to the numbers, operation, and command keys. To evaluate
    the equation, I used a class constructor that bundles the operator,
    first operand, and second operand for the operate function.
    
    Additionally, I added a modulo operation for the operate function.
    
    I also added a separate function that would display the calculator
    results into the HTML calculator display by displaying the values inside
    the global variable holders.
    
    I also added a class to make the placeholder input of the calculator
    display hidden from view until the user inputs a number.

--- script.js
+++ script.js
@@ -1,31 +1,93 @@
-const add = function add (firstOperand, secondOperand) {
-    return firstOperand + secondOperand;
+const getCalculatorInputs = function getCalculatorInputs (targetButton) {
+    const numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+    const operationList = ["mod", "÷", "×", "–", "+", "( )"];
+    const commandList = ["C", "CE", "="];
+
+    // Check if the first input is an operator instead of a number
+    if (operationList.includes(targetButton.textContent) && firstOperandHolder === "") return alert("Invalid format.")
+    if (commandList.includes(targetButton.textContent) && firstOperandHolder === "") return
+
+    if (numberList.includes(targetButton.textContent)) {
+        if (firstOperandHolder !== "" && operatorHolder !== "") {
+            secondOperandHolder += targetButton.textContent;
+        } else {
+            firstOperandHolder += targetButton.textContent;
+        }
+        
+    } else if (operationList.includes(targetButton.textContent) && firstOperandHolder !== "") {
+        operatorHolder = targetButton.textContent;
+    }
+
+    if (targetButton.textContent === "=") {
+        class NumericalData {
+            constructor(operator, firstOperand, secondOperand) {
+                this.operator = operator;
+                this.firstOperand = firstOperand;
+                this.secondOperand = secondOperand;
+            }
+        }
+        
+        const numericalData = new NumericalData(operatorHolder, Number(firstOperandHolder), Number(secondOperandHolder));
+        
+        // Reset the contents of the holders
+        firstOperandHolder = "";
+        operatorHolder = "";
+        secondOperandHolder = "";
+
+        firstOperandHolder = operate(numericalData);
+    }
+
+    // Populate calculator display with result
+    displayCalculatorResult();
 }
 
-const subtract = function subtract (firstOperand, secondOperand) {
-    return firstOperand - secondOperand;
-}
+const operate = function operate (numericalData) {
+    const add = function add (firstOperand, secondOperand) {
+        return firstOperand + secondOperand;
+    }
+    
+    const subtract = function subtract (firstOperand, secondOperand) {
+        return firstOperand - secondOperand;
+    }
+    
+    const multiply = function multiply (firstOperand, secondOperand) {
+        return firstOperand * secondOperand;
+    }
+    
+    const divide = function divide (firstOperand, secondOperand) {
+        if (secondOperand === 0) return "Can't divide by zero."
+        return firstOperand / secondOperand;
+    }
+    
+    const modulo = function modulo (firstOperand, secondOperand) {
+        if (secondOperand === 0) return "Can't divide by zero."
+        return firstOperand % secondOperand;
+    }
 
-const multiply = function multiply (firstOperand, secondOperand) {
-    return firstOperand * secondOperand;
-}
-
-const divide = function divide (firstOperand, secondOperand) {
-    if (secondOperand === 0) return "Can't divide by zero."
-    return firstOperand / secondOperand;
-}
-
-const operate = function operate (operator, firstOperand, secondOperand) {
-    switch (operator) {
-        case "+": return add(firstOperand, secondOperand);
-        case "-": return subtract(firstOperand, secondOperand);
-        case "*": return multiply(firstOperand, secondOperand);
-        case "/": return divide(firstOperand, secondOperand);
+    switch (numericalData.operator) {
+        case "+": return String(add(numericalData.firstOperand, numericalData.secondOperand));
+        case "–": return String(subtract(numericalData.firstOperand, numericalData.secondOperand));
+        case "×": return String(multiply(numericalData.firstOperand, numericalData.secondOperand));
+        case "÷": return String(divide(numericalData.firstOperand, numericalData.secondOperand));
+        case "mod": return String(modulo(numericalData.firstOperand, numericalData.secondOperand));
     }
 }
 
-console.log(operate("+", 3, 5));
-console.log(operate("-", 3, 5));
-console.log(operate("*", 3, 5));
-console.log(operate("/", 1, 0));
-console.log(operate("/", 0, 1));
+const displayCalculatorResult = function displayCalculatorResult () {
+    let textDisplay = document.querySelector("p.text-display");
+
+    if (textDisplay.classList.contains("hidden")) {
+        textDisplay.classList.remove("hidden");
+    }
+
+    textDisplay.textContent = `${firstOperandHolder} ${operatorHolder} ${secondOperandHolder}`;
+}
+
+const keyButtons = document.querySelectorAll("button.key");
+let operatorHolder = "";
+let firstOperandHolder = "";
+let secondOperandHolder = "";
+
+keyButtons.forEach( (button) => {
+    button.addEventListener("click", () => getCalculatorInputs(button));
+});
