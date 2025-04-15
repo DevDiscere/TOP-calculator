e36be720298286c3b8147cebca6223e231e159b0
DevDiscere <dev.ginomiguel@gmail.com>
2025-04-15 14:03:11 +0800

    Modify calculator logic to handle edge cases
    I modified the calculator logic by handling the following edge cases:
    
    1. To make the calculator only evaluate a single pair of numbers at a
    time, I added a logic that checks if all three global variables are not
    null, then I run the operate function and assign the result back to the
    first operand and then sets the operator to the user's next operator.
    This handles the chaining of calculator operations cleanly.
    
    2. To avoid overflowing the display with long decimals, I added the
    formatStringPrecision function to only set the value to 10 decimal
    places, remove trailing zeroes, and convert the result to string.
    
    3. To replace the displayed result when the user presses a new digit, I
    added a logic that would check if the previous expression was evaluated.
    
    4. To make sure that the calculator only runs an operation with one
    operator, I added a logic similar to No. 1. I also made sure that if the
    user clicks a new operator, it would replace the previous operator.
    
    5. Prevent '=' from running the operate function if not all global
    variables are filled with input.
    
    6. Prevent and warn user from dividing by zero.
    
    7. Add a clear button that wipes out all data within the global
    variables.
    
    8. Prevent multiple decimal points and multiple zeroes before a decimal
    point.
    
    9. Add zero to the beginning of a decimal point if the user did not set
    a number before inputting decimal point.
    
    10. Add a clear entry button that removes the last input from the global
    variables.

--- script.js
+++ script.js
@@ -1,44 +1,8 @@
-const getCalculatorInputs = function getCalculatorInputs (targetButton) {
-    const numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-    const operationList = ["mod", "÷", "×", "–", "+", "( )"];
-    const commandList = ["C", "CE", "="];
-
-    // Check if the first input is an operator instead of a number
-    if (operationList.includes(targetButton.textContent) && firstOperandHolder === "") return alert("Invalid format.")
-    if (commandList.includes(targetButton.textContent) && firstOperandHolder === "") return
-
-    if (numberList.includes(targetButton.textContent)) {
-        if (firstOperandHolder !== "" && operatorHolder !== "") {
-            secondOperandHolder += targetButton.textContent;
-        } else {
-            firstOperandHolder += targetButton.textContent;
-        }
-        
-    } else if (operationList.includes(targetButton.textContent) && firstOperandHolder !== "") {
-        operatorHolder = targetButton.textContent;
-    }
-
-    if (targetButton.textContent === "=") {
-        class NumericalData {
-            constructor(operator, firstOperand, secondOperand) {
-                this.operator = operator;
-                this.firstOperand = firstOperand;
-                this.secondOperand = secondOperand;
-            }
-        }
-        
-        const numericalData = new NumericalData(operatorHolder, Number(firstOperandHolder), Number(secondOperandHolder));
-        
-        // Reset the contents of the holders
-        firstOperandHolder = "";
-        operatorHolder = "";
-        secondOperandHolder = "";
-
-        firstOperandHolder = operate(numericalData);
-    }
-
-    // Populate calculator display with result
-    displayCalculatorResult();
+const clearHolderValues = function clearHolderValues () {
+    operatorHolder = "";
+    firstOperandHolder = "";
+    secondOperandHolder = "";
+    isEquationEvaluated = false;
 }
 
 const operate = function operate (numericalData) {
@@ -55,39 +19,159 @@ const operate = function operate (numericalData) {
     }
     
     const divide = function divide (firstOperand, secondOperand) {
-        if (secondOperand === 0) return "Can't divide by zero."
         return firstOperand / secondOperand;
     }
     
     const modulo = function modulo (firstOperand, secondOperand) {
-        if (secondOperand === 0) return "Can't divide by zero."
         return firstOperand % secondOperand;
     }
 
+    const formatStringPrecision = function formatStringPrecision (numberValue) {
+        // Set numberValue to fixed-notation of 10 decimal places,
+        // Then parse the string as float to remove trailing zeroes,
+        // Then convert the floating value to string.
+        return String(Number.parseFloat(numberValue.toFixed(10)));
+    }
+
     switch (numericalData.operator) {
-        case "+": return String(add(numericalData.firstOperand, numericalData.secondOperand));
-        case "–": return String(subtract(numericalData.firstOperand, numericalData.secondOperand));
-        case "×": return String(multiply(numericalData.firstOperand, numericalData.secondOperand));
-        case "÷": return String(divide(numericalData.firstOperand, numericalData.secondOperand));
-        case "mod": return String(modulo(numericalData.firstOperand, numericalData.secondOperand));
+        case "+": return formatStringPrecision(add(numericalData.firstOperand, numericalData.secondOperand));
+        case "–": return formatStringPrecision(subtract(numericalData.firstOperand, numericalData.secondOperand));
+        case "×": return formatStringPrecision(multiply(numericalData.firstOperand, numericalData.secondOperand));
+        case "÷": return formatStringPrecision(divide(numericalData.firstOperand, numericalData.secondOperand));
+        case "mod": return formatStringPrecision(modulo(numericalData.firstOperand, numericalData.secondOperand));
     }
 }
 
-const displayCalculatorResult = function displayCalculatorResult () {
+const populateCalculatorDisplay= function populateCalculatorDisplay(choice) {
     let textDisplay = document.querySelector("p.text-display");
 
-    if (textDisplay.classList.contains("hidden")) {
+    // Set text display to hidden if C is clicked or first operand is empty
+    if (choice === "clear" || firstOperandHolder === "") {
+        textDisplay.classList.add("hidden")
+        textDisplay.textContent = "0";
+    } else {
         textDisplay.classList.remove("hidden");
+        textDisplay.textContent = `${firstOperandHolder} ${operatorHolder} ${secondOperandHolder}`;
+    }
+}
+
+const processCalculatorInput = function processCalculatorInput (targetButtonInput) {
+    class Expression {
+        constructor(firstOperand, operator, secondOperand) {
+            this.firstOperand = firstOperand;
+            this.operator = operator;
+            this.secondOperand = secondOperand;
+        }
     }
 
-    textDisplay.textContent = `${firstOperandHolder} ${operatorHolder} ${secondOperandHolder}`;
+    const numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+    const operationList = ["mod", "÷", "×", "–", "+", "( )"];
+    const commandList = ["C", "CE", "="];
+
+    // Early return if input is anything but a number
+    if (operationList.includes(targetButtonInput) && firstOperandHolder === "") return alert("Invalid format.");
+    if (commandList.includes(targetButtonInput) && firstOperandHolder === "") return;
+
+    // First operand
+    if (numberList.includes(targetButtonInput) && operatorHolder === "") {
+        // TASK: Consider decimal part, it should not be included in the 15 digits.
+        if (firstOperandHolder.length >= 15) return alert("Can't enter more than 15 digits.");
+        // Prevent trailing zeroes before decimal point
+        if (targetButtonInput === "0" && firstOperandHolder.at(0) === "0" && !firstOperandHolder.includes(".")) return;
+        // Prevent multiple decimal points
+        if (targetButtonInput === "." && firstOperandHolder.includes(".")) return;
+        // Overwrites the evaluated result when user clicks a new number
+        if (isExpressionEvaluated) {     
+            // Prepends zero if operand holder does not have decimal point
+            if (targetButtonInput === "." && !firstOperandHolder.includes(".")) {
+                firstOperandHolder = "0" + targetButtonInput;
+            } else {
+                firstOperandHolder = targetButtonInput;
+            }
+
+            isExpressionEvaluated = false;  
+        } else {
+            if (targetButtonInput === "." && firstOperandHolder === "") {
+                firstOperandHolder = "0" + targetButtonInput;
+            } else if (targetButtonInput !== "0" && targetButtonInput !== "." && firstOperandHolder === "0") {
+                firstOperandHolder = targetButtonInput;
+            } else {
+                firstOperandHolder += targetButtonInput;
+            } 
+        }
+    }
+
+    // Operator
+    if (operationList.includes(targetButtonInput)) {
+        if ((operatorHolder === "mod" || operatorHolder === "÷") && secondOperandHolder === "0") return alert("Can't divide by zero.");
+        // Chain operation
+        if (firstOperandHolder !== ""  && operatorHolder !== "" && secondOperandHolder !== "") {
+            const calculatorExpression = new Expression(Number(firstOperandHolder), operatorHolder, Number(secondOperandHolder));
+            
+            clearHolderValues();
+            firstOperandHolder = operate(calculatorExpression);
+            isExpressionEvaluated = true;
+        }
+
+        operatorHolder = targetButtonInput;
+    }
+
+    // Second Operand
+    if (numberList.includes(targetButtonInput) && operatorHolder !== "") {
+        // TASK: Consider decimal part, it should not be included in the 15 digits.
+        if (secondOperandHolder.length >= 15) return alert("Can't enter more than 15 digits.");
+        // Prevent trailing zeroes before decimal point
+        if (targetButtonInput === "0" && secondOperandHolder.at(0) === "0" && !secondOperandHolder.includes(".")) return;
+        // Prevent multiple decimal points
+        if (targetButtonInput === "." && secondOperandHolder.includes(".")) return;
+
+        if (targetButtonInput === "." && secondOperandHolder === "") {
+            secondOperandHolder = "0" + targetButtonInput;
+        } else if (targetButtonInput !== "0" && targetButtonInput !== "." && secondOperandHolder === "0") {
+            secondOperandHolder = targetButtonInput;
+        } else {
+            secondOperandHolder += targetButtonInput;
+        }
+    }
+
+    // Equal Button
+    if (targetButtonInput === "=" && firstOperandHolder !== "" && operatorHolder !== "") {
+        if (secondOperandHolder === "") return alert("Invalid format.");
+        if ((operatorHolder === "mod" || operatorHolder === "÷") && secondOperandHolder === "0") return alert("Can't divide by zero.");
+
+        const calculatorExpression = new Expression(Number(firstOperandHolder), operatorHolder, Number(secondOperandHolder));
+        
+        clearHolderValues();
+        firstOperandHolder = operate(calculatorExpression);
+        isExpressionEvaluated = true;
+    }
+
+    // Clear: Clear all inputs of the calculator
+    if (targetButtonInput === "C") {
+        clearHolderValues();
+        return populateCalculatorDisplay("clear");
+    }
+
+    // Clear Entry: Removes the last input from the expression
+    if (targetButtonInput == "CE") {
+        if (operatorHolder === "" && secondOperandHolder === "") {
+            firstOperandHolder = firstOperandHolder.slice(0, firstOperandHolder.length - 1);
+        } else if (firstOperandHolder !== "" && secondOperandHolder === "") {
+            operatorHolder = operatorHolder.slice(0, operatorHolder.length - 1);
+        } else if (firstOperandHolder !== "" && operatorHolder !== "") {
+            secondOperandHolder = secondOperandHolder.slice(0, secondOperandHolder.length - 1);
+        }
+    }
+
+    populateCalculatorDisplay();
 }
 
 const keyButtons = document.querySelectorAll("button.key");
 let operatorHolder = "";
 let firstOperandHolder = "";
 let secondOperandHolder = "";
+let isExpressionEvaluated = false;
 
 keyButtons.forEach( (button) => {
-    button.addEventListener("click", () => getCalculatorInputs(button));
+    button.addEventListener("click", () => processCalculatorInput(button.textContent));
 });
